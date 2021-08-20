c7196c0b6aef7141af2e612a199a0cc5d0115759
seunghunHan1 <hkgb0009@gmail.com>
2021-08-20 17:52:59 +0900

    django폼

--- second/views.py
+++ second/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from second.models import Post
+from .forms import PostForm
 
 # Create your views here.
 def list(request):
@@ -7,3 +9,14 @@ def list(request):
         'items' : Post.objects.all()
     }
     return render( request, 'second/list.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_form = form.save()
+        return HttpResponseRedirect('/second/list/')
+
+    form = PostForm()
+    return render(request, 'second/create.html', {'form': form})
